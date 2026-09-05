962de409c951810306e0dce1ff011ebabb3e29a4
ZAIN-UL-ABDIN-GHANI <175481804+ZAIN-UL-ABDIN-GHANI@users.noreply.github.com>
2026-09-05 11:55:22 +0500

    feat(api): add Axios request interceptor for automated Bearer token attachment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,6 +9,16 @@ const client: AxiosInstance = axios.create({
   },
 })
 
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
 client.interceptors.response.use(
   (response) => response,
   (error) => {
